e5e326fa617428729ee8f92816f9b3c447d18759
nosianuri <nurinosia@gmail.com>
2023-04-09 00:12:51 +0600

    update loader image size

--- src/Shared/Loader.js
+++ src/Shared/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-[#e48a0340]">
-      <div className=" rounded  w-32  border-gray-900 !leading-normal">
+      <div className=" rounded  w-64  border-gray-900 !leading-normal">
         <img src="https://i.ibb.co/WFZvYCD/zip-window.png" alt="" />
       </div>
     </div>
--- src/Shared/Mloader.js
+++ src/Shared/Mloader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Mloader = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-[#e48a0340]">
-      <div className=" rounded w-32 border-gray-900 !leading-normal">
+      <div className=" rounded w-64 border-gray-900 !leading-normal">
         <img src="https://i.ibb.co/LCGZhhZ/home-service.png" alt="" />
       </div>
     </div>
