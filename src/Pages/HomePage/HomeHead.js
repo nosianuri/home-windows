13ccb5a47a0e57711e6b785f3f2a8a333b317a71
nosianuri <nurinosia@gmail.com>
2023-04-19 02:14:48 +0600

    update

---
+++ src/Pages/HomePage/HomeHead.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeHead = () => {
+  return (
+    <div className='bg-[#f6f6f6]'>
+        <div className='max-w-7xl mx-auto'>
+        <div>
+        <img src="https://i.ibb.co/LCGZhhZ/home-service.png" alt="" />
+            <img src="https://i.ibb.co/yFv6tCK/awards.png" alt="" />
+        </div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default HomeHead
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeHead from './HomeHead'
+
+const HomePage = () => {
+  return (
+    <div>
+        <HomeHead />
+    </div>
+  )
+}
+
+export default HomePage
