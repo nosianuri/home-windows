86311bca1d9dab7b9791e323b84764f96e3318e4
nosianuri <nurinosia@gmail.com>
2023-04-20 22:11:36 +0600

    update professional img

--- src/Pages/Aboutus/AboutBanner.js
+++ src/Pages/Aboutus/AboutBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutBanner = () => {
     return (
         <div style={{
-            background: `url(https://i.ibb.co/zPFWK0V/female-construction-worker-PM3-R974-scaled.jpg)`
+            background: `url(https://i.ibb.co/PNpjzdh/about-us.png)`
         }} className="bg-no-repeat !bg-cover">
             <div className=" bg-black bg-opacity-60">
                 <div className='text-[#fff] text-center py-16 px-2'>
--- src/Pages/HomePage/Professional.js
+++ src/Pages/HomePage/Professional.js
@@ -124,98 +124,98 @@ const Professional = () => {
         {
             id: 1,
             name: 'Roofing',
-            img: 'https://i.ibb.co/YNc3BGX/bg12.png',
+            img: 'https://i.ibb.co/F45bhkf/home-service2.png',
             link: '/',
             desc: 'Our roofing services are licensed and insured, and we offer competitive pricing and free estimates.',
         },
         {
             id: 2,
             name: 'Window',
-            img: 'https://i.ibb.co/9ZCn0ZH/bg14.png',
+            img: 'https://i.ibb.co/YQv3qLQ/home-service3.png',
             link: '/',
             desc: 'Our window services are backed by a satisfaction guarantee, and we offer free estimates for all our projects.',
         },
         {
             id: 3,
             name: 'Solar',
-            img: 'https://i.ibb.co/vDcXnPD/bg13.png',
+            img: 'https://i.ibb.co/XFJ2hCK/home-service4.png',
             link: '/',
             desc: 'Our solar services are affordable and customizable to fit your needs, and we offer financing options to make solar energy more accessible to homeowners.',
         },
         {
             id: 4,
             name: 'Plumbing',
-            img: 'https://i.ibb.co/YDfgRjn/bg1.png',
+            img: 'https://i.ibb.co/rZPv65W/home-service5.png',
             link: '/',
             desc: 'Our plumbers can help you unclog stubborn drains in your home. Whether its a kitchen sink or shower drain.',
         },
         {
             id: 5,
             name: 'Movers',
-            img: 'https://i.ibb.co/QvZqQHG/bg10.png',
+            img: 'https://i.ibb.co/4scGSCd/home-service6.png',
             link: '/',
             desc: 'Our moving services are licensed and insured, and we offer competitive pricing and flexible scheduling options.',
         },
         {
             id: 6,
             name: 'Kitchen',
-            img: 'https://i.ibb.co/2Fy6xVh/bg7.png',
+            img: 'https://i.ibb.co/q9rQCwj/home-service7.png',
             link: '/',
             desc: 'Our kitchen services are customized to your needs, and we offer financing options for larger projects.',
         },
         {
             id: 7,
             name: 'HVA',
-            img: 'https://i.ibb.co/ccFfP6W/bg2.png',
+            img: 'https://i.ibb.co/j3QjSH6/home-service8.png',
             link: '/',
             desc: 'Our HVAC services are backed by a money-back guarantee, and we offer financing options for larger projects.',
         },
         {
             id: 8,
             name: 'Home Security',
-            img: 'https://i.ibb.co/VNNvQ3q/bg9.png',
+            img: 'https://i.ibb.co/PNtRctN/home-service9.png',
             link: '/',
             desc: 'Protect your home and family with our state-of-the-art alarm systems. We offer both wired and wireless options to fit your homes layout.',
         },
         {
             id: 9,
             name: 'Gutter',
-            img: 'https://i.ibb.co/JtznkJg/bg6.png',
+            img: 'https://i.ibb.co/MBdP81n/home-service-10.png',
             link: '/',
             desc: 'Keep your homes gutters free of debris and prevent water damage with our gutter cleaning services. We can also install gutter guards to reduce the need for future cleaning.',
         },
         {
             id: 10,
             name: 'Home Painting',
-            img: 'https://i.ibb.co/HhmWrh1/bg5.png',
+            img: 'https://i.ibb.co/F6RqyPV/home-service1.png',
             link: '/',
             desc: 'Whether you want to refresh your homes style or prepare it for sale, our professional painters can give your interior walls a fresh coat of paint with a variety of colors and finishes.',
         },
         {
             id: 11,
             name: 'Home Cleaning',
-            img: 'https://i.ibb.co/3RnwVtn/bg4.png',
+            img: 'https://i.ibb.co/zf5HfPQ/home-service-11.png',
             link: '/',
             desc: 'Our team can provide a thorough deep cleaning of your home, including hard-to-reach areas like baseboards and high shelves.',
         },
         {
             id: 12,
             name: 'Flooring',
-            img: 'https://i.ibb.co/CmN45Dx/bg11.png',
+            img: 'https://i.ibb.co/41MccLR/home-service12.png',
             link: '/',
             desc: 'Add warmth and elegance to your home with our hardwood flooring installation services. We offer a variety of wood types and finishes to fit your style and budget.',
         },
         {
             id: 13,
             name: 'Electrical',
-            img: 'https://i.ibb.co/bRnJCV9/bg3.png',
+            img: 'https://i.ibb.co/hyTZM0W/home-service13.png',
             link: '/',
             desc: 'Upgrade your homes lighting with our professional installation services. We can install anything from ceiling fans to dimmer switches to help you save energy and enhance your homes ambiance.',
         },
         {
             id: 14,
             name: 'Bathroom',
-            img: 'https://i.ibb.co/vZqnM6n/bg8.png',
+            img: 'https://i.ibb.co/hy6Y7bb/home-service14.png',
             link: '/',
             desc: 'Upgrade your bathrooms style and functionality with a new shower installation. We offer a range of styles and features to fit your needs and preferences.',
         },
@@ -233,7 +233,7 @@ const Professional = () => {
                                         <a href={city.link}>
                                         <div className='mt-5'>
                                             <div className="w-[35%] mx-auto">
-                                                    <img src="https://i.ibb.co/gMsx4JM/cropped-favicon-180x180.png" alt='' className='w-24 rounded-full absolute mt-[-50px] !mx-auto' />
+                                                    <img src={city.img} alt='' className='w-24 rounded-full absolute mt-[-50px] !mx-auto' />
                                             </div>
                                             {/* <div> 
                                         <img className='w-8' src={city.img} alt="" />
